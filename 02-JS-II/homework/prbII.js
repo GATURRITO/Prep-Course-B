d63b912686d694fbe73d83f845a78439b06ca2de
GATURRITO <sheik0309@gmail.com>
2022-12-03 14:23:40 -0500

    homework JSII resuelta

---
+++ 02-JS-II/homework/prbII.js
@@ -0,0 +1,171 @@
+// function sumarDosNumeros (a, b){
+//     var suma = a + b;
+// return suma;
+// }
+// var sumitaNumeros = sumarDosNumeros(100, 200);
+// console.log(sumitaNumeros);
+
+// function añoNacim(edad){
+// var añoActual = 2022;
+// var edad = "63"; // no importa se es string
+// console.log(añoActual-edad);
+
+
+// } 
+// añoNacim();
+
+// function multiplica(x, y) {
+//     // Multiplica "x" por "y" y devuelve el valor
+//     // Tu código:
+//     var multiplicador = x * y;
+//     return multiplicador;
+
+//   }
+//   console.log (multiplica(4, 5));
+
+
+
+// function tienenMismaLongitud(str1, str2) {
+//     if(str1.length === str2.length) {return true}
+//     else {
+//         return false;
+//     }
+//     }
+
+    // function menosQueNoventa(num) {
+    //     // Devuelve "true" si el argumento de la función "num" es menor que noventa
+    //     // De lo contrario, devuelve "false"
+    //     // Tu código:
+    //     if(num < 90){
+    //         return true
+    //     }
+    //     else {
+    //         return false
+    //     }
+    // }
+    // function obtenerResto(x, y) {
+    //     // Obten el resto de la división de "x" entre "y"
+    //     // Tu código:
+    //     var sobrante = x % y
+    //     return sobrante
+    // }
+
+    // function esPar(num) {
+    //     // Devuelve "true" si "num" es par
+    //     // De lo contrario, devuelve "false"
+    //     // Tu código:
+    //     if(num % 2 === 0) {
+    //         return true
+    //     }
+    //     else{
+    //         return false
+    //     }
+
+    // }
+
+    // function esImpar(num) {
+    //     // Devuelve "true" si "num" es impar
+    //     // De lo contrario, devuelve "false"
+    //     // Tu código:
+    //     if(num % 2 !== 0) {
+    //         return true
+    //     }
+    //     else{
+    //         return false
+    //     }
+
+    // }
+
+    // 
+    
+//   function redondearNumero(num) {
+//     // Redondea "num" al entero más próximo y devuélvelo
+//     // Tu código:
+
+//     return Math.round (num)
+
+//   }
+
+// function redondearHaciaArriba(num) {
+//     // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
+//     // Tu código:
+//     return Math.ceil (num);
+
+//   }
+//   console.log (redondearHaciaArriba(4.42))
+
+//   function redondearHaciaAbajo(num) {
+//     // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
+//     // Tu código:
+//     return Math.floor (num);
+
+//   }
+//   console.log (redondearHaciaAbajo(4.82))
+
+// function numeroRandom() {
+//     //Generar un número al azar entre 0 y 1 y devolverlo
+//     //Pista: investigá qué hace el método Math.random()
+//     // Tu código:
+// return Math.random()
+// }
+// console.log (numeroRandom ()); cada resultado >0 pero menor que 1
+
+
+
+    // function esPositivo(numero) {
+        //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo.
+        //Si el número es positivo, devolver ---> "Es positivo"
+        //Si el número es negativo, devolver ---> "Es negativo"
+        //Si el número es 0, devuelve false
+        // Tu código:
+        // if(numero > 0){
+        //    return "Es positivo"
+        // }
+        // else if(numero < 0) {
+        //      return "Es negativo"
+        //  }
+        // else if(numero === 0) {
+        //             return false
+        //         }
+           
+        // }
+    
+
+// }
+
+
+
+// function agregarSimboloExclamacion(str) {
+//     // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
+//     // Ejemplo: "hello world" pasaría a ser "hello world!"
+//     // Tu código:
+//     var adic = str + "!";
+//     return adic;
+
+///}
+// function combinarNombres(nombre, apellido) {
+//     // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
+//     // Ejemplo: "Soy", "Henry" -> "Soy Henry"
+//     // Tu código:
+
+//     return nombre + " " + apellido;
+
+// }
+
+// function obtenerSaludo(nombre) {
+//     // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
+//     // "Martin" -> "Hola Martin!"
+//     // Tu código:
+
+// var saludo = "Hola" + " " +  nombre + "!";
+// return saludo;
+
+//}
+// console.log(obtenerSaludo(Martin))
+
+// function obtenerAreaRectangulo(alto, ancho) {
+//     // Retornar el area de un rectángulo teniendo su altura y ancho
+//     // Tu código:
+//     return alto * ancho;
+
+// }
